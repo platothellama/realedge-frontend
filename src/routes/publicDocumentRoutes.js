ac2dbac0e01f6a21d0909ed5c63c3d84286b0c08
Ramy Merheb <platothellama@gmail.com>
2026-03-27 22:17:21 +0200

    test

--- src/routes/publicDocumentRoutes.js
+++ src/routes/publicDocumentRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const documentController = require('../controllers/documentController');
 
-router.get('/sign/:documentId/:token', documentController.getPublicSigningData);
-router.post('/sign/:documentId/:token', documentController.processPublicSignature);
+router.get('/:documentId/:token', documentController.getPublicSigningData);
+router.post('/:documentId/:token', documentController.processPublicSignature);
 
 module.exports = router;
