5fd57623cae47d6179385dbc300d271ef143a5df
Ramy Merheb <platothellama@gmail.com>
2026-04-02 14:42:34 +0300

    test

--- src/routes/groupRoutes.js
+++ src/routes/groupRoutes.js
@@ -11,6 +11,7 @@ router.get('/stats', groupController.getGroupStats);
 router.get('/:id/members', groupController.getGroupMembers);
 router.post('/', groupController.createGroup);
 router.patch('/:id', groupController.updateGroup);
+router.put('/:id', groupController.updateGroup);
 router.delete('/:id', groupController.deleteGroup);
 router.post('/members', groupController.addGroupMember);
 
